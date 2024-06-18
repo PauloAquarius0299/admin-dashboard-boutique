3386282c92e496aced71f4830b807c6d0aec706f
Paulo Cesar <paulocgsantos99@gmail.com>
2024-06-18 14:48:52 -0300

    fetch collections editor and id

--- components/collections/CollectionsForm.tsx
+++ components/collections/CollectionsForm.tsx
@@ -1,92 +1,93 @@
-"use client"
+"use client";
 
-import {z} from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { useRouter } from "next/navigation";
 
-import { Separator } from '../ui/separator';
+import { Separator } from "../ui/separator";
 import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "../ui/textarea";
 import ImageUpload from "../custom ui/ImageUplaod";
-import { useState } from 'react';
+import { useState } from "react";
 import toast from "react-hot-toast";
 import Delete from "../custom ui/Delete";
 
 const formSchema = z.object({
-    title: z.string().min(2).max(20),
-    description: z.string().min(2).max(500).trim(),
-    image: z.string(),
-})
+  title: z.string().min(2).max(20),
+  description: z.string().min(2).max(500).trim(),
+  image: z.string(),
+});
 
-interface CollectionFormProps{
-    initialData?: CollectionType | null;
+interface CollectionFormProps {
+  initialData?: CollectionType | null; 
 }
 
+const CollectionForm: React.FC<CollectionFormProps> = ({ initialData }) => {
+  const router = useRouter();
 
-const CollectionsForm: React.FC<CollectionFormProps> = ({initialData})=> {
-    const router = useRouter()
+  const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false)
-
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
+      ? initialData
+      : {
           title: "",
-          description: '',
-          image: '',
+          description: "",
+          image: "",
         },
-      })
+  });
 
-      const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement> | React.KeyboardEvent<HTMLTextAreaElement>)=> {
-        if(e.key === 'Enter'){
-            e.preventDefault();
-        }
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement> | React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  }
+  
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      const url = initialData
+        ? `/api/collections/${initialData._id}`
+        : "/api/collections";
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(values),
+      });
+      if (res.ok) {
+        setLoading(false);
+        toast.success(`Collection ${initialData ? "updated" : "created"}`);
+        window.location.href = "/collections";
+        router.push("/collections");
       }
+    } catch (err) {
+      console.log("[collections_POST]", err);
+      toast.error("Algo deu errado! Por favor tente novamente.");
+    }
+  };
 
-      const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        try {
-            setLoading(true);
-            const url = initialData
-              ? `/api/collections/${initialData._id}`
-              : "/api/collections";
-            const res = await fetch(url, {
-              method: "POST",
-              body: JSON.stringify(values),
-            });
-            if (res.ok) {
-              setLoading(false);
-              toast.success(`Collection ${initialData ? "updated" : "created"}`);
-              window.location.href = "/collections";
-              router.push("/collections");
-            }
-          } catch (err) {
-            console.log("[collections_POST]", err);
-            toast.error("Something went wrong! Please try again.");
-          }
-        };
-
-    return (
-        <div className='p-10'>
-            {initialData ? (
-                <div className='flex items-center justify-between'>
-                    <p className='text-heading2-bold'>Editar Coleção</p>
-                    <Delete id={initialData._id} item="collection" />
-                </div>
-            ) : (
-                 <p className='text-heading2-bold'>Criar Coleções</p>
-            )}
-            <Separator className='bg-grey-1 mt-4 mb-7' />
-            <Form {...form}>
+  return (
+    <div className="p-10">
+      {initialData ? (
+        <div className="flex items-center justify-between">
+          <p className="text-heading2-bold">Editar Coleção</p>
+          <Delete id={initialData._id} item="collection" />
+        </div>
+      ) : (
+        <p className="text-heading2-bold">Criar Coleção</p>
+      )}
+      <Separator className="bg-grey-1 mt-4 mb-7" />
+      <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
             control={form.control}
@@ -95,26 +96,26 @@ const CollectionsForm: React.FC<CollectionFormProps> = ({initialData})=> {
               <FormItem>
                 <FormLabel>Titulo</FormLabel>
                 <FormControl>
-                  <Input placeholder="Titulo" {...field}  onKeyDown={handleKeyPress}  />
+                  <Input placeholder="Titulo" {...field} onKeyDown={handleKeyPress} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-           <FormField
+          <FormField
             control={form.control}
             name="description"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Descrinção</FormLabel>
                 <FormControl>
-                  <Textarea placeholder="Descrinção" {...field} rows={5} onKeyDown={handleKeyPress} />
+                  <Textarea placeholder="Descreva sobre o estilo..." {...field} rows={5} onKeyDown={handleKeyPress} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-         <FormField
+          <FormField
             control={form.control}
             name="image"
             render={({ field }) => (
@@ -122,31 +123,31 @@ const CollectionsForm: React.FC<CollectionFormProps> = ({initialData})=> {
                 <FormLabel>Imagem</FormLabel>
                 <FormControl>
                   <ImageUpload
-                  value={field.value ? [field.value] : []}
-                  onChange={(url) => field.onChange(url)}
-                  onRemove={() => field.onChange("")}
+                    value={field.value ? [field.value] : []}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
                   />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <div className='flex gap-10'>
-            <Button type='submit' className='bg-purple-600 text-white'>
-                Enviar
+          <div className="flex gap-10">
+            <Button type="submit" className="bg-purple-600 text-white">
+              Enviar
             </Button>
             <Button
-            type='button'
-            className='bg-blue-1 text-white'
-            onClick={() => router.push("/collections")}
+              type="button"
+              onClick={() => router.push("/collections")}
+              className="bg-blue-1 text-white"
             >
-                Discartar
+              Discartar
             </Button>
           </div>
-          </form>
-          </Form>
-        </div>
-    )
-}
+        </form>
+      </Form>
+    </div>
+  );
+};
 
-export default CollectionsForm;
+export default CollectionForm;
